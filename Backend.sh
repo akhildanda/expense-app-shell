f004f263d069c8b07b6df5720bf3bc5c1b99fe82
akhildanda <akhildanda7@gmail.com>
2024-04-22 15:02:15 +0530

    hello script

---
+++ Backend.sh
@@ -0,0 +1,48 @@
+#!bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+      echo -e "$2...$R FAILURE $N"
+      exit 1
+    else
+      echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+    if [ $USERID -ne 0 ]
+    then
+      echo "PLEASE RUN THE SCRIPT WITH ROOT ACCESS."
+      exit 1
+    else
+      echo "YOU ARE SUPER USER."
+    fi
+
+    dnf module disable nodejs -y &>>$LOGFILE
+    VALIDATE $? "Disabling default nodejs"
+
+    dnf module enable nodejs:20 -y &>>$LOGFILE
+    VALIDATE $? "Enabling nodejs:20 version"
+
+    dnf install nodejs -y &>>$LOGFILE
+    VALIDATE $? "Installing nodejs"
+
+    id expense &>>$LOGFILE
+    if [$? -ne 0]
+    then
+       useradd expense &>>$LOGFILE
+       VALIDATE $? "Creating expense user"
+    else
+       echo -e "Expense user already created...$Y SKIPPING $N"
+    fi
+
+    
